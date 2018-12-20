ee19f157653aca836bd7879a6f57113b49c539f5
JDB <legodude17@users.noreply.github.com>
2018-12-20 08:44:31 -0800

    Add default to this

--- tasks/folder.js
+++ tasks/folder.js
@@ -5,7 +5,14 @@ module.exports = {
     {
       type: 'input',
       name: 'folder',
-      message: 'Where should it be?'
+      message: 'Where should it be?',
+      initial(input) {
+        const hash = input.state.answers;
+        if (hash.pkgjson && hash.pkgjson.values.name) return hash.pkgjson.values.name;
+        if (hash.pkgjson && hash.pkgjson.values.repo) return hash.pkgjson.values.repo;
+        if (hash.name) return hash.name;
+        return '';
+      }
     }
   ],
   task: {
