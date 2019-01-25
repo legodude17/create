7e57fe45c89e856d27827d3a1b5457895d22bd94
JDB <legodude17@users.noreply.github.com>
2019-01-25 14:09:01 -0800

    Fix bugs in typer

--- typer.js
+++ typer.js
@@ -8,10 +8,19 @@ module.exports = function typer(types) {
   return {
     questions: [
       {
-        type: 'list',
+        type: 'select',
         name: 'type',
         message: 'What type of project is this? ',
         choices: types.map(type => type.type)
+      },
+      {
+        type: 'input',
+        name: 'entry',
+        message: 'What is the entrypoint?',
+        initial(input) {
+          const { type } = input.state.answers;
+          return types.filter(t => t.type === type)[0].entry;
+        }
       }
     ].concat(types.reduce(
       (arr, type) => arr.concat(type.questions.map(question => Object.assign(
@@ -30,15 +39,28 @@ module.exports = function typer(types) {
     ).concat({
       when: () => true,
       name: 'install',
-      desc: 'Install Packages',
+      title: 'Install Packages',
       run(answers, ll, utils) {
-        const { packages } = types.filter(type => type.type === answers.type)[0];
+        const { packages } = types.filter(type => type.type === answers.type)[0](answers).filter(Boolean);
         packages.forEach(pkg => ll.addTask({ name: pkg, title: `Install ${pkg}` }));
         return Promise.all(packages
           .map(pkg => utils.execa.shell(`npm i -D ${pkg}`))
           .map((prom, i) =>
             prom.then(() => ll[packages[i]].complete('Installed'))))
-          .then(() => answers);
+          .then(() => 'Installed all packages');
+      }
+    }).concat({
+      when: () => true,
+      name: 'entry',
+      title: 'Create Entrypoint',
+      run(answers, ll, utils) {
+        const { entry } = answers;
+        ll.addTask({ name: 'folder', title: 'Make folder' });
+        const folder = utils.path.dirname(entry);
+        return utils.mkdirp(folder).then(() => {
+          ll.folder.complete(`Created folder ${folder}`);
+          return utils.writeFile(entry, '// ENTRYPOINT');
+        }).then(() => `Created ${entry}`);
       }
     })
   };
@@ -50,7 +72,8 @@ type is:
   type: '',
   questions: [],
   packages: [],
-  tasks: []
+  tasks: [],
+  entry: ''
 }
 
 */
