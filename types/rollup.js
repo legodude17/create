b59c49ae84d52d461f9fa53d15a46193791515b9
JDB <legodude17@users.noreply.github.com>
2019-01-25 14:09:20 -0800

    Add rollup type

---
+++ types/rollup.js
@@ -0,0 +1,42 @@
+module.exports = {
+  type: 'rollup',
+  questions: [{
+    type: 'confirm',
+    name: 'babel',
+    message: 'Use babel?'
+  }],
+  packages: answers => ['rollup', 'rollup-plugin-node-resolve', answers.babel && 'rollup-plugin-babel@beta'],
+  tasks: [{
+    name: 'rollupConfig',
+    title: 'Create rollup config file',
+    run(answers, tl, utils) {
+      const rollupConfig = [
+        'import resolve from \'rollup-plugin-node-resolve\';',
+        answers.babel && 'import babel from \'rollup-plugin-babel\';',
+        '',
+        'export default {',
+        `  input: '${answers.entry.replace(/ /g, '')}',`,
+        '  output: {',
+        '  file: \'dist/bundle.js\',',
+        '    format: \'cjs\'',
+        '  },',
+        '  plugins: [',
+        '    resolve(),',
+        answers.babel && '    babel()',
+        '  ]',
+        '}'
+      ].filter(Boolean).join('\n');
+      return utils.writeFile('rollup.config.js', rollupConfig).then(() => 'Wrote to rollup.config.js');
+    }
+  }, {
+    name: 'babelrc',
+    title: 'Create babel config file',
+    run(answers, tl, utils) {
+      const babelrc = {
+        presets: [['env', { modules: false }]]
+      };
+      return utils.write('.babelrc', babelrc).then(() => 'Wrote to .babelrc');
+    }
+  }],
+  entry: 'src/index.js'
+};
